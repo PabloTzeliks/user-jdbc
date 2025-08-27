84cb6f73865730acf8406af6e44844f13aca0d8a
PabloTzeliks <arq.pabloo@gmail.com>
2025-08-26 21:33:30 -0300

    PRT-002 iniial: estrutura de pacotes inicial

--- src/main/java/pablo/tzeliks/model/User.java
+++ src/main/java/pablo/tzeliks/model/User.java
@@ -1,4 +1,9 @@
 package pablo.tzeliks.model;
 
 public class User {
+
+    private String nome;
+    private String email;
+    private String password;
+
 }
--- src/main/java/pablo/tzeliks/repository/ConnectDatabase.java
+++ src/main/java/pablo/tzeliks/repository/ConnectDatabase.java
@@ -1,4 +1,18 @@
 package pablo.tzeliks.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectDatabase {
+
+    private static final String URL = "jdbc:mysql://localhost:3356/mysqlDB?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mysqlPW";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+
+
 }
